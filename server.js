fbecf8ac5fbcb4f36834924ec1e1be96d0969745
Darren Porciello <darren.porciello1@gmail.com>
2023-11-24 23:05:29 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files (HTML, CSS, JS, and SVG)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Handle download request
+app.post('/download', express.json(), (req, res) => {
+    const { text, filename } = req.body;
+  
+    // Implement logic to save the text to a file (e.g., speech.txt)
+    // You can use fs module or any other method of your choice
+  
+    res.json({ success: true });
+  });
